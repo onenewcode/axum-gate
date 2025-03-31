de6d7ff8c798019574051cb32e103a94d641a701
Lewin Probst <info@emirror.de>
2025-03-31 16:33:33 +0200

    wip: Added initial project setup with Credentials struct

---
+++ src/credentials.rs
@@ -0,0 +1,20 @@
+use serde::{Deserialize, Serialize};
+
+/// Defines credentials for a simple login based on an `id` and a `secret`.
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Credentials {
+    /// The identification of the user, eg. a username.
+    pub id: String,
+    /// The secret of the user, eg. a password.
+    pub secret: String,
+}
+
+impl Credentials {
+    /// Creates a new ticket with the given id and secret.
+    pub fn new(id: &str, secret: &str) -> Self {
+        Self {
+            id: id.to_string(),
+            secret: secret.to_string(),
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+//! Role based JWT auth for axum.
+#![deny(missing_docs)]
+
+mod credentials;
+
+pub use credentials::Credentials;
