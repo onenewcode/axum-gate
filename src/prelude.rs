2e26d5e7396bd605a012b45a341389de324e628d
Lewin Probst <info@emirror.de>
2025-10-17 15:54:37 +0200

    chore: Extended prelude by RegisteredClaims and Permissions

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,11 +1,11 @@
 //! Common types and functions for quick imports.
 pub use crate::accounts::Account;
 pub use crate::authz::AccessPolicy;
-pub use crate::codecs::jwt::{JsonWebToken, JsonWebTokenOptions, JwtClaims};
+pub use crate::codecs::jwt::{JsonWebToken, JsonWebTokenOptions, JwtClaims, RegisteredClaims};
 pub use crate::cookie_template::CookieTemplateBuilder;
 pub use crate::credentials::Credentials;
 pub use crate::gate::Gate;
 pub use crate::groups::Group;
 pub use crate::jsonwebtoken::{DecodingKey, EncodingKey};
-pub use crate::permissions::PermissionId;
+pub use crate::permissions::{PermissionId, Permissions};
 pub use crate::roles::Role;
