2745a1487007862ddfcf24523407715225efaf41
Lewin Probst <info@emirror.de>
2025-10-17 15:54:37 +0200

    chore: Moved prelude to separate file

--- src/lib.rs
+++ src/lib.rs
@@ -276,17 +276,4 @@ pub mod secrets;
 pub mod verification_result;
 
 pub mod errors;
-
-/// Common types and functions for quick imports.
-pub mod prelude {
-    pub use crate::accounts::Account;
-    pub use crate::authz::AccessPolicy;
-    pub use crate::codecs::jwt::{JsonWebToken, JsonWebTokenOptions, JwtClaims};
-    pub use crate::cookie_template::CookieTemplateBuilder;
-    pub use crate::credentials::Credentials;
-    pub use crate::gate::Gate;
-    pub use crate::groups::Group;
-    pub use crate::jsonwebtoken::{DecodingKey, EncodingKey};
-    pub use crate::permissions::PermissionId;
-    pub use crate::roles::Role;
-}
+pub mod prelude;
